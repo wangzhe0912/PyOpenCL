8c3c6eab5bc55f11a8912bdd4322b0db651af088
wangzhe0912 <wangzhe0912@tju.edu.cn>
2016-09-04 10:00:51 +0800

    versionQueries

---
+++ versionQueries.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import pyopencl
+
+print pyopencl.VERSION
+print pyopencl.VERSION_STATUS
+print pyopencl.VERSION_TEXT
+print pyopencl.get_cl_header_version()
+
+#raise pyopencl.Error
+#raise pyopencl.MemoryError
+#raise pyopencl.LogicError
+#raise pyopencl.RuntimeError
